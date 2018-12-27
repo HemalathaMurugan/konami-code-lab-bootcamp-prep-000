df1ab2f994b194a747ccb5325cbc0e9647b5f8a9
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-27 22:02:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,27 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const codes=[38,38,40,40,37,39,37,39,66,65];
+function init(){
+  let done=0;
+  let nd=document.querySelector('body');
+  nd.addEventListener('keydown',function(e){ 
+    
+    let k=e.which;
+    console.log(k);
+    if(k===codes[done]){
+      
+      done++;
+      console.log(done);
+    
+    if(done===codes.length){
+      alert('Congrats!');
+      done=0;
+    } 
+      
+    }
+    else {
+      done=0;
+    }
+  });
 
-function init() {
-  // your code here
 }
+
+  
